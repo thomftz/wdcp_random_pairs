e32cd4eefa11a938316ace67ee2a74e2b3dc7600
Thomas Fitzpatrick <t_f@me.com>
2017-07-18 09:15:46 -0400

    Refactoring Random Victim

--- random_victim.rb
+++ random_victim.rb
@@ -1,15 +1,7 @@
-cohort_10 = ["Chad Keller", "Bradley Broge", "Oakley Peavler", "Zack Clark",
-"Sara Wall", "Michael Prechter", "Dylan Andrew Blake", "Lula Goodwin",
-"Matthew McDonald", "Robert Garmhausen", "Eric Kanarr", "Tim Minge",
-"Ethan Jarrell", "Gary Williams", "Ethan Philipson", "Zoe Cooper",
-"Francis Walsh", "Matt Morgan", "Geoff Humphreys", "Michael Ashton",
-"Brittany Arsi", "Oresti Arsi", "Sam Caldwell", "Lorien Olive",
-"Steve Roma", "Amelia Royster", "Sean Shellie", "Jason Grigg",
-"Robert Hutchin"]
-
-cohort_11 = ["Christopher Perry", "Keith Canty", "Colin Di Meo", "Robin Butler", "Justin Kim", "KarenLee Herron", "Jared Patterson", "Sam Steele", "Jonathan Canfield", "Tivona Spruill", "Lyman Johnson", "Hannah Hudson", "Ginnie Hench", "Michael Ivy", "Kevin Harrison", "John Knapp", "Nancy Belscher", "Aaron Anderson", "Katy Dillard", "Brett Shirley", "Jocelyn Triplet"]
+require_relative 'cohort_10.rb'
+require_relative 'cohort_11.rb'
 
 puts "Which Cohort? (10 or 11)"
 cohort = gets.to_i
 
-cohort == 10 ? (p cohort_10.sample) : (p cohort_11.sample)
+cohort == 10 ? (p $cohort_10.sample) : (p $cohort_11.sample)

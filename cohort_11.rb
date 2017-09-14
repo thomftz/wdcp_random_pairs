73d3d7d7793a805890c2d8ca9c117b13f4a78c02
Thomas Fitzpatrick <t_f@me.com>
2017-09-14 09:57:51 -0400

    Updating class transfers

--- cohort_11.rb
+++ cohort_11.rb
@@ -4,7 +4,7 @@ $cohort_11 = ["Christopher Perry", "Keith Canty", "Colin Di Meo", "Robin Butler"
   "Katy Dillard", "Brett Shirley", "Jocelyn Triplet"]
 
   $ruby_students = ["Christopher Perry", "Keith Canty", "Colin Di Meo", "Lyman Johnson",
-  "Hannah Hudson", "Ginnie Hench", "John Knapp", "Nancy Belscher", "Brett Shirley", "Jocelyn Triplet"]
+  "Robin Butler", "Ginnie Hench", "John Knapp", "Nancy Belscher", "Brett Shirley", "Jocelyn Triplet"]
 
-  $react_students = ["Jared Patterson", "Sam Steele", "Jonathan Canfield", "Tivona Spruill", "Hannah Hudson",
+  $react_students = ["Hannah Hudson", "Jared Patterson", "Sam Steele", "Jonathan Canfield", "Tivona Spruill", "Hannah Hudson",
      "Kevin Harrison", "Aaron Anderson", "Katy Dillard"]
